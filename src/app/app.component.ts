4fa5d58a1b0e0fab8dd895d72ade5e94a4106f7c
Mike <michael.jenni.83@hotmail.com>
2021-01-03 20:23:17 +0100

    second Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NgModel } from '@angular/forms';
+import { MoviesService } from './movies.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  filter= '';
+  constructor(
+    private movies: MoviesService,
+  ) {}
+  ngOnInit() {
+
+  }
+
+  search(filter) {
+    this.movies.emitChange(filter);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,40 @@
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StorageService } from './storage.service';
+import { MovieComponent } from './movie/movie.component';
+import { MovieDetailsComponent } from './movie/movie-details/movie-details.component';
+import { SearchPipe } from './search.pipe';
+import { MoviesService } from './movies.service';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+
+const routes: Routes = [
+  {path: '', component: MoviesListComponent},
+  {path: ':category', component: MoviesListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    SearchPipe,
+    MoviesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [
+    StorageService,
+    MoviesService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
